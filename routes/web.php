1be0f36274063b32149e666b162d83445d7d060b
mathiasCarlier <mathiasc1811@gmail.com>
2024-10-07 11:13:10 +0200

    hello

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/hello', function(){
+    return 'Bonjour le monde ! ';
+});
+
 require __DIR__.'/auth.php';
