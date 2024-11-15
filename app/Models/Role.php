2d03e3c7d12a4d9d322eb146ba3dcf2b5212b35d
mathiasCarlier <mathiasc1811@gmail.com>
2024-11-15 11:20:44 +0100

    ajout

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,30 +6,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 /**
  * Class Role
  * 
- * @property int $id
+ * @property int $idRole
  * @property string $label
- * 
- * @property Collection|User[] $users
  *
  * @package App\Models
  */
 class Role extends Model
 {
-	protected $table = 'PFX_roles';
+	protected $table = 'LC_roles';
+	protected $primaryKey = 'idRole';
 	public $timestamps = false;
 
 	protected $fillable = [
 		'label'
 	];
-
-	public function users()
-	{
-		return $this->hasMany(User::class, 'id_role');
-	}
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,6 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-
 /**
  * Class User
  * 
@@ -24,78 +20,27 @@ use Illuminate\Notifications\Notifiable;
  * @property string|null $remember_token
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
- * @property string|null $first_name
- * @property string|null $last_name
- * @property string|null $comment
- * @property string $code_gender
- * @property int $id_role
- * 
- * @property Gender $gender
- * @property Role $role
  *
  * @package App\Models
  */
-#class User extends Model
-class User extends Authenticatable
+class User extends Model
 {
-	use HasFactory, Notifiable;
-
-	#protected $table = 'PFX_users';
+	protected $table = 'LC_users';
 
 	protected $casts = [
-		'email_verified_at' => 'datetime',
-		'id_role' => 'int'
+		'email_verified_at' => 'datetime'
 	];
 
-	/**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
 	protected $hidden = [
 		'password',
 		'remember_token'
 	];
 
-
-	/**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-	 */
 	protected $fillable = [
 		'name',
 		'email',
 		'email_verified_at',
 		'password',
-		'remember_token',
-		'first_name',
-		'last_name',
-		'comment',
-		'code_gender',
-		'id_role'
+		'remember_token'
 	];
-
-	public function gender()
-	{
-		return $this->belongsTo(Gender::class, 'code_gender');
-	}
-
-	public function role()
-	{
-		return $this->belongsTo(Role::class, 'id_role');
-	}
-
-	/**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
 }
